74d6e7cad076b97b8558cbb2bdb428b4aa6fedcc
Chaeho Lee <ch.chell39@gmail.com>
2025-03-03 01:09:32 +0900

    [level 2] Title: H-Index, Time: 0.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,11 @@
 def solution(citations):
     citations.sort(reverse=True)
+    
     for i, c in enumerate(citations):
-        if c < i+1:
+        print(f"{i}, {c} if {c} < {i+1}")
+        # 조건 1: h 번 이상 인용된 논문이 h 편 이상
+        # 조건 2: 나머지 논문이 h 번 이하 인용
+        if c <= i:
             return i
+        
     return len(citations) 
