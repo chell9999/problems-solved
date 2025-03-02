438f8e7c297058f70329235a191bdc3288dfdfcf
Chaeho Lee <ch.chell39@gmail.com>
2025-03-02 23:51:44 +0900

    [level 2] Title: 가장 큰 수, Time: 57.77 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,22 +1,12 @@
 def solution(numbers):
+    str_nums = list(map(str, numbers))
+    
 
-    str_numbers = list(map(str, numbers))
-
-    from functools import cmp_to_key
-
-    def compare(x, y):
-        if x + y > y + x:
-            return -1  
-        elif x + y < y + x:
-            return 1  
-        else:
-            return 0  
-
-    str_numbers.sort(key=cmp_to_key(compare))
-
-    answer = ''.join(str_numbers)
-
+    str_nums.sort(key=lambda x: x*3, reverse=True)
+   
+    answer = ''.join(str_nums)
+    
     if answer[0] == '0':
-        answer = '0'
-
+        return '0'
+    
     return answer
